aed3eddb26a6e20f721eedd829c2891a0f00906c
Jakob Roberts <jakob.m.roberts@gmail.com>
2016-02-20 23:53:51 +0000

    Next Iteration: 2016-02-20 23:53:51

--- --backup1.py
+++ --backup1.py
@@ -339,24 +339,30 @@ def move():
                 possibleFoods.append(tuple(food))
                     
         #Go to closest food
-        closestFoodDist = 0
-        closestFood = None
-        for food in possibleFoods:
-            d = manDist(tuple(ourSnake['coords'][0]), food)
-            if d < closestFoodDist or closestFood == None:
-                closestFood = food
-                closestFoodDist = d
-                
-        idle = False
-        if closestFood != None:
-            path = aStar(grid, tuple(ourSnake['coords'][0]), closestFood)
-            if path != False and not isPositionBetter(grid, ourSnake, tuple(ourSnake['coords'][0]), path, closestFood): #not position better? whaaa
-                move = directions[path.direction()]
-                print "Food>> " + move
+        havedest = False
+        while not havedest:
+            closestFoodDist = 0
+            closestFood = None
+            for food in possibleFoods:
+                d = manDist(tuple(ourSnake['coords'][0]), food)
+                if d < closestFoodDist or closestFood == None:
+                    closestFood = food
+                    closestFoodDist = d
+                    
+            idle = False
+            
+            if closestFood != None:
+                path = aStar(grid, tuple(ourSnake['coords'][0]), closestFood)
+                if path != False and not isPositionBetter(grid, ourSnake, tuple(ourSnake['coords'][0]), path, closestFood): #not position better? whaaa
+                    move = directions[path.direction()]
+                    print "Food>> " + move
+                    havedest = True
+                else:
+                    possibleFoods.remove(tuple(closestFood))
+                    idle = True
             else:
+                havedest = True
                 idle = True
-        else:
-            idle = True
         
     # IDLE ACTIONS
     simpleMovements = False
